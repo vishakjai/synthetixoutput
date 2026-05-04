2409c444a0d1ab68d929d724b1e830d9f66ae6f4
vishakjai <50501795+vishakjai@users.noreply.github.com>
2026-05-04 05:08:42 -0500

    Synthetix run 20260504_012903_ef3812da: export synthetix/runs/20260504_012903_ef3812da/qa/qa/authmodule/src/handlers.go

--- synthetix/runs/20260504_012903_ef3812da/qa/qa/authmodule/src/handlers.go
+++ synthetix/runs/20260504_012903_ef3812da/qa/qa/authmodule/src/handlers.go
@@ -2,17 +2,7 @@ package main
 
 import "net/http"
 
-func ConfirmProfilePictureRequestHandler(w http.ResponseWriter, r *http.Request) {
+func ExecuteHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"status": "ConfirmProfilePictureRequest executed"}`))
-}
-
-func PictureUploadResponseHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"status": "PictureUploadResponse executed"}`))
-}
-
-func DeleteMessageDtoHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"status": "DeleteMessageDto executed"}`))
+	w.Write([]byte(`{"status": "Executed"}`))
 }
